1675d93580a186c0382aaabbc212b7ca12cf884f
Luan <luan_uchoa@outlook.com>
2025-09-27 18:29:39 -0400

    Replace a few stray URLs

--- pdsadmin.sh
+++ pdsadmin.sh
@@ -3,7 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-PDSADMIN_BASE_URL="https://raw.githubusercontent.com/bluesky-social/pds/main/pdsadmin"
+#PDSADMIN_BASE_URL="https://raw.githubusercontent.com/bluesky-social/pds/main/pdsadmin"
+PDSADMIN_BASE_URL="https://raw.githubusercontent.com/luan-u/bsky-pds/refs/heads/caramelo/pdsadmin"
 
 # Command to run.
 COMMAND="${1:-help}"
